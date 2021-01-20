e3d935bee74bb3ac7d6501e1540256ad0b66b4b8
Qian Chen <QuokeCola@iCloud.com>
2021-01-20 19:33:52 +0800

    Add Data Summary

--- LA1/Plotter.py
+++ LA1/Plotter.py
@@ -1,11 +1,11 @@
 import matplotlib.pyplot as plt
 class DataPlotter:
-    times = []
-    input = []
-    output = []
-    Filename = ''
-    datast = ['time,input,output\n']
     def __init__(self, filename) -> None:
+        self.times = []
+        self.input = []
+        self.output = []
+        self.Filename = ''
+        self.datast = ['time,input,output\n']
         super().__init__()
         self.Filename = filename
         raw_data = open(self.Filename,'r').readlines()
@@ -54,10 +54,59 @@ class DataPlotter:
         file.close
         
         # Plot
+        plt.title(self.Filename.strip('Data/'))
         plt.plot(self.times[:self.times.index(endInd)], self.input[:self.times.index(endInd)], label = 'input')
         plt.plot(self.times[:self.times.index(endInd)], self.output[:self.times.index(endInd)], label = 'output')
         plt.legend()
         plt.show()
+    def find(self):
+        RiseInd = []
+        DownInd = []
+        for i in range(0,len(self.output)):
+            if (self.output[i] - 3.0) * (self.output[min(len(self.output)-1,i+1)]-3.0) < 0:
+                if self.output[i] < 3.0 and self.output[min(len(self.output)-1,i+1)] >3.0:
+                    RiseInd.append(i)
+                else:
+                    DownInd.append(i)
+        Test = self.times[DownInd[1]] - self.times[RiseInd[1]]
+        if Test > 0:
+            switch = False
+        else:
+            switch = True
+        HTime = 0.0
+        DTime = 0.0
+        count = 0
+        for i in range(1,min(len(RiseInd),len(DownInd))-2):
+            if switch:
+                HTime += (self.times[DownInd[i]] - self.times[RiseInd[i-1]])
+                DTime += (self.times[RiseInd[i]] - self.times[DownInd[i]])
+            else:
+                HTime += (self.times[DownInd[i]] - self.times[RiseInd[i]])
+                DTime += (self.times[RiseInd[i+1]] - self.times[DownInd[i]])
+            count += 1 
+        dutyCycle = HTime/(HTime+DTime)
+        AvgHt = HTime/count
+        AvgDt = DTime/count
+        AvgLt = (HTime+DTime)/count
 
-Data = DataPlotter('Data/10k_2')
-Data.plot()
+        # print('dutyC = '+ str(dutyCycle))
+        # print('AvgHt = '+ str(AvgHt))
+        # print('AvgDt = '+ str(AvgDt))
+        # print('AvgLt = '+ str(AvgLt))
+        
+        return {'dutyCycle'                 :dutyCycle, 
+                'Average High Level Time'   :AvgHt, 
+                'Average Low Level Time'    :AvgDt, 
+                'Average Cycle Time'        :AvgLt}
+Data10k_1 = DataPlotter('Data/10k_1')
+Data10k_2 = DataPlotter('Data/10k_2')
+Data20k_1 = DataPlotter('Data/20k_1')
+Data20k_2 = DataPlotter('Data/20k_2')
+Res = [Data10k_1.find(), Data10k_2.find(), Data20k_1.find(), Data20k_2.find()]
+Outputres = open('Results.csv','w+')
+OutputLine = []
+for i in Res:
+    OutputLine.append("{}, {}, {}, {}\n".format(i['dutyCycle'],i['Average High Level Time'],i['Average Low Level Time'],i['Average Cycle Time']))
+    print("{}, {}, {}, {}\n".format(i['dutyCycle'],i['Average High Level Time'],i['Average Low Level Time'],i['Average Cycle Time']))
+Outputres.writelines(OutputLine)
+#Data.plot(2)
